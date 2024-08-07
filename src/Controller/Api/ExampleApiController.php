d3683176aed87a14e02601f1eaf10da3ada55743
Akellymourab <anakellymourab@gmail.com>
2024-08-07 18:53:16 -0300

    Implements internationalization in the views with a "Hello World" example.

--- src/Controller/Api/ExampleApiController.php
+++ src/Controller/Api/ExampleApiController.php
@@ -5,13 +5,19 @@ declare(strict_types=1);
 namespace App\Controller\Api;
 
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Contracts\Translation\TranslatorInterface;
 
 class ExampleApiController
 {
+    public function __construct(
+        private readonly TranslatorInterface $translator
+    ) {
+    }
+
     public function getList(): JsonResponse
     {
         return new JsonResponse([
-            'message' => 'hello world',
+            'message' => $this->translator->trans('hello world'),
         ]);
     }
 }
