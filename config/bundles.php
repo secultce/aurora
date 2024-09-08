5f88fcdaac310ad090f42ab9bada72d949202d63
Henrique Lopes Lima <38414109+henriquelopeslima@users.noreply.github.com>
2024-09-08 10:00:05 -0300

    Add the DAMA (#96)

--- config/bundles.php
+++ config/bundles.php
@@ -14,4 +14,5 @@ return [
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     League\FlysystemBundle\FlysystemBundle::class => ['all' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
+    DAMA\DoctrineTestBundle\DAMADoctrineTestBundle::class => ['test' => true],
 ];
