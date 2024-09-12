8d4c6af85f086a6b56c8e7b7d83eba3b8326cb3d
Alessandro Feitoza <alessandrofeitoza01@gmail.com>
2024-09-12 11:19:01 -0300

    Fix import map for all style files (#129)

--- assets/app.js
+++ assets/app.js
@@ -5,6 +5,18 @@
  * which should already be in your base.html.twig.
  */
 import './vendor/aurora-user-interface/dist/css/bootstrap.min.css';
+import './vendor/aurora-user-interface/dist/css/colors.css';
+import './vendor/aurora-user-interface/dist/css/layout.css';
+
+import './vendor/aurora-user-interface/dist/css/accordion.css.css';
+import './vendor/aurora-user-interface/dist/css/card.css';
+import './vendor/aurora-user-interface/dist/css/editor.css';
+import './vendor/aurora-user-interface/dist/css/faq.css';
+import './vendor/aurora-user-interface/dist/css/forms.css';
+import './vendor/aurora-user-interface/dist/css/navigation.css';
+import './vendor/aurora-user-interface/dist/css/side-filter.css';
+import './vendor/aurora-user-interface/dist/css/timeline.css';
+
 import './vendor/aurora-user-interface/dist/css/custom.css';
 import './styles/app.css';
 
