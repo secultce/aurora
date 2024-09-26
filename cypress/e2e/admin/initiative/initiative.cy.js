d53d8154a430cf3e9c4b5d1b78302564b5672795
Ana Kelly Moura Balbino <101601082+Akellymourab@users.noreply.github.com>
2024-09-26 12:16:46 -0300

    Updates the dashboard class name. (#156)

---
+++ cypress/e2e/admin/initiative/initiative.cy.js
@@ -0,0 +1,32 @@
+describe('Página de Lista de Iniciativas', () => {
+    beforeEach(() => {
+        cy.viewport(1920, 1080);
+        cy.visit('/iniciativas');
+    });
+
+    it('Garante que a dashboard de iniciativas está visível', () => {
+        // Verifica se a dashboard está visível
+        cy.get('.entity-dashboard-initiative').should('be.visible');
+    });
+
+    it('Garante que a página de lista de iniciativas existe', () => {
+        cy.get('span.name-one').contains('Início').should('be.visible');
+        cy.get('span.name-one').contains('Iniciativa').should('be.visible');
+        cy.get('h2.page-title').contains('Iniciativa').should('be.visible');
+    });
+
+    it('Garante que o botão "Criar uma Iniciativa" está visível', () => {
+        cy.get('button').contains('Criar iniciativa').should('be.visible');
+    });
+
+    it('Garante que a lista de iniciativas está presente e os itens são visíveis', () => {
+        cy.get('.initiative-card').should('be.visible');
+        cy.get('.initiative-card').first().should('be.visible');
+    });
+
+    it('Garante que cada iniciativa tem botão de acessar visível', () => {
+        cy.get('.initiative-card').each(($el) => {
+            cy.wrap($el).find('a:contains("Acessar"), button:contains("Acessar")').should('be.visible');
+        });
+    });
+});
