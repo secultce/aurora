a00f84a4aee6c63b2cfa7419a7f805dd39a53319
Alessandro Feitoza <alessandrofeitoza01@gmail.com>
2024-09-29 13:05:22 -0300

    Remove unnecessary layouts

--- cypress/e2e/admin/agent/agent-list.cy.js
+++ cypress/e2e/admin/agent/agent-list.cy.js
@@ -8,15 +8,12 @@ describe('Página de Listar de Agentes', () => {
         cy.get('.breadcrumb-agent > :nth-child(1)').contains('Inicio').should('be.visible');
         cy.get('.breadcrumb-agent > :nth-child(2)').contains('Agentes').should('be.visible');
         cy.get('.entity-dashboard-title').contains('Agentes').should('be.visible');
+        cy.contains('Agentes Cadastrados');
+        cy.contains('Alice Nogueira');
     });
 
     it('Garante que as opções de visualização estão presentes', () => {
         cy.get('a').contains('Lista').should('be.visible');
         cy.get('a').contains('Mapa').should('be.visible');
-    });
-
-    it('Garante que a lista de agentes está presente e os itens são visíveis', () => {
-        cy.get('.agent-list').should('be.visible');
-        cy.get('.agent-list > :nth-child(2)').first().should('be.visible');
     });
 });
