21564265c9078c570d848ec16aed93fbe0e982b0
Akellymourab <anakellymourab@gmail.com>
2024-10-02 11:43:00 -0300

    Adds registration page 1.

---
+++ assets/js/form-stepper.js
@@ -0,0 +1,51 @@
+const prevBtns = document.querySelectorAll('.btn-prev');
+const nextBtns = document.querySelectorAll('.btn-next');
+const progress = document.getElementById('progress');
+const formSteps = document.querySelectorAll('.form-step');
+const progressSteps = document.querySelectorAll('.progress-step');
+
+let formStepsNum = 0;
+
+nextBtns.forEach((btn) => {
+    btn.addEventListener("click", () => {
+        formStepsNum++;
+        updateFormSteps();
+        updateProgressbar();
+    });
+});
+
+prevBtns.forEach((btn) => {
+    btn.addEventListener("click", () => {
+        formStepsNum--;
+        updateFormSteps();
+        updateProgressbar();
+    });
+});
+
+function updateFormSteps() {
+    formSteps.forEach((formStep) => {
+        formStep.classList.remove('form-step-active');
+    });
+
+    formSteps[formStepsNum].classList.add('form-step-active');
+};
+
+function updateProgressbar() {
+    const totalSteps = progressSteps.length - 1;
+    const activeSteps = formStepsNum + 1;
+
+    progressSteps.forEach((progressStep, index) => {
+        progressStep.classList.toggle("progress-step-active", index < activeSteps);
+    });
+
+    const progressPercentage = (activeSteps - 1) / totalSteps * 100;
+    progress.style.width = `${progressPercentage}%`;
+}
+
+progressSteps.forEach((progressStep, index) => {
+    progressStep.addEventListener('click', () => {
+        formStepsNum = index;
+        updateFormSteps();
+        updateProgressbar();
+    });
+});
--- cypress/e2e/admin/authentication/register.cy.js
+++ cypress/e2e/admin/authentication/register.cy.js
@@ -3,13 +3,21 @@ describe('Página de Cadastro', () => {
         cy.visit('/cadastro');
     });
 
+    it('Clica no botão Voltar e verifica redirecionamento para a página inicial', () => {
+        cy.contains('a', 'Voltar').click();
+
+        cy.url().should('eq', `${Cypress.config().baseUrl}`);
+    });
+
     it('Verifica se a página de cadastro existe', () => {
         cy.title().should('include', 'Cadastro');
 
         cy.get('form.form-stepper').should('exist').and('be.visible');
 
         cy.get('#inputName').should('exist');
+        cy.get('#inputBirdDate').should('exist');
         cy.get('#inputCPF').should('exist');
+        cy.get('#inputPhone').should('exist');
         cy.get('#inputEmail').should('exist');
         cy.get('#inputPassword').should('exist');
         cy.get('#inputPasswordConfirm').should('exist');
@@ -17,18 +25,14 @@ describe('Página de Cadastro', () => {
 
     it('Preenche os inputs e clica em Continuar', () => {
         cy.get('#inputName').type('João da Silva');
+        cy.get('#inputBirdDate').type('1990-01-01');
         cy.get('#inputCPF').type('123.456.789-10');
+        cy.get('#inputPhone').type('(11) 91234-5678');
         cy.get('#inputEmail').type('joao.silva@exemple.com');
         cy.get('#inputPassword').type('senha123');
         cy.get('#inputPasswordConfirm').type('senha123');
 
         cy.get('.btn').contains('Continuar').click();
 
-    });
-
-    it('Clica no botão Voltar e verifica redirecionamento para a página inicial', () => {
-        cy.contains('a', 'Voltar').click();
-
-        cy.url().should('eq', `${Cypress.config().baseUrl}`);
     });
 });
