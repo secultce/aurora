484880be83ede61d26f6d88e7a300435690a2b32
Talyson Soares <talysonsoares40@gmail.com>
2024-10-06 09:53:51 -0300

    implement database data on agents listing page (#195)

--- src/Controller/Admin/AgentAdminController.php
+++ src/Controller/Admin/AgentAdminController.php
@@ -4,12 +4,35 @@ declare(strict_types=1);
 
 namespace App\Controller\Admin;
 
+use App\Service\Interface\AgentServiceInterface;
 use Symfony\Component\HttpFoundation\Response;
 
 class AgentAdminController extends AbstractAdminController
 {
+    public function __construct(
+        public readonly AgentServiceInterface $service,
+    ) {
+    }
+
     public function list(): Response
     {
-        return $this->render('agent/list.html.twig');
+        $agents = $this->service->list();
+        $totalAgents = count($agents);
+
+        $entity = [
+            'color' => '#D0A020',
+            'items' => [
+                ['text' => 'agent.registered_agents', 'icon' => 'description', 'quantity' => $totalAgents],
+                ['text' => 'agent.individual_agents', 'icon' => 'event_note', 'quantity' => 32],
+                ['text' => 'agent.collective_agents', 'icon' => 'event_available', 'quantity' => 12],
+                ['text' => 'agent.registered_last_7_days', 'icon' => 'today', 'quantity' => 1],
+            ],
+        ];
+
+        return $this->render('agent/list.html.twig', [
+            'agent' => $entity,
+            'agents' => $agents,
+            'totalAgents' => $totalAgents,
+        ]);
     }
 }
