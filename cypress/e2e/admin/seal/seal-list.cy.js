21f4985cdef42101c0891546582032540cc7154c
TalysonSoares <talysonsoares40@gmail.com>
2024-10-08 08:50:22 -0300

    Seals Management Page

---
+++ cypress/e2e/admin/seal/seal-list.cy.js
@@ -0,0 +1,39 @@
+describe('Página de listar Selos', () => {
+    beforeEach(() => {
+        cy.viewport(1920, 1080);
+        cy.visit('/selos');
+    });
+
+    it('Garante que a página de selos existe', () => {
+        cy.get('.management-panel__title').contains('Gestão de Selos').should('be.visible');
+        cy.get('.management-panel__action').contains('Criar selo').should('be.visible');
+    });
+
+    it('Garante que as tabs estão funcionando', () => {
+        const tabs = [
+            {tab: '#pills-published-tab'},
+            {tab: '#pills-draft-tab'},
+            {tab: '#pills-allowed-tab'},
+            {tab: '#pills-archived-tab'},
+            {tab: '#pills-deleted-tab'}
+        ];
+
+        tabs.forEach(({ tab}) => {
+            cy.get(tab).click();
+            cy.get(tab).should('have.class', 'active');
+        })
+    });
+
+    it('Garante que o input existe', () => {
+        cy.get('#search-seal').should('be.visible');
+    })
+
+    it('Garante que os cards estão visíveis', () => {
+        cy.get(':nth-child(2) > .seals-header > img').should('be.visible');
+        cy.get(':nth-child(2) > .seals-header > img').should('be.visible');
+        cy.get(':nth-child(2) > .justify-content-between > .actions-manage > :nth-child(1)').should('be.visible');
+        cy.get(':nth-child(2) > .justify-content-between > .actions-manage > :nth-child(2)').should('be.visible');
+        cy.get(':nth-child(2) > .justify-content-between > .actions-interact > .seals-edit').should('be.visible');
+        cy.get(':nth-child(2) > .justify-content-between > .actions-interact > :nth-child(2)').should('be.visible');
+    });
+})
