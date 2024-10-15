dd645b6aaeac3af9ab5b63ea1c515d0b004a01c0
Sara Camilo <saraccamilo@gmail.com>
2024-10-15 12:13:04 -0300

    separate css aurora

--- assets/app.js
+++ assets/app.js
@@ -11,7 +11,22 @@ import 'aurora-user-interface/dist/css/navigation.css';
 import 'aurora-user-interface/dist/css/side-filter.css';
 import 'aurora-user-interface/dist/css/timeline.css';
 
+import './styles/pages/homepage.css';
+import './styles/pages/seals.css';
+import './styles/pages/agents.css';
+import './styles/pages/events.css';
+import './styles/pages/initiatives.css';
+import './styles/pages/faq.css';
+import './styles/pages/not-found.css';
+import './styles/pages/spaces.css';
+
 import './styles/app.css';
 import './styles/components/navbar.css';
+import './styles/components/footer.css';
+import './styles/components/breadcrumb.css';
+import './styles/components/dashboard.css';
+import './styles/components/side-bar.css';
+import './styles/components/title.css';
+import './styles/components/form-step.css';
 
 import 'aurora-user-interface/dist/js/bootstrap.min.js';
