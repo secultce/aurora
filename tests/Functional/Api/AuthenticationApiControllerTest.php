7374ce53f4ac9df4cbdec84c04530db5667f35dc
TalysonSoares <talysonsoares40@gmail.com>
2024-10-23 13:30:40 -0300

    Create logout endpoint

--- tests/Functional/Api/AuthenticationApiControllerTest.php
+++ tests/Functional/Api/AuthenticationApiControllerTest.php
@@ -12,6 +12,8 @@ use Symfony\Component\HttpFoundation\Response;
 class AuthenticationApiControllerTest extends AbstractWebTestCase
 {
     private const string BASE_URL = '/api/login';
+    private const string AGENT_URL = '/api/agents';
+    private const string LOGOUT_URL = '/api/logout';
 
     public function testCanLogin(): void
     {
@@ -55,6 +57,58 @@ class AuthenticationApiControllerTest extends AbstractWebTestCase
             'details' => [
                 'The credentials you provided are invalid. Please try again.',
             ],
+        ]);
+    }
+
+    private function loginAndGetToken($client, string $username, string $password): string
+    {
+        $requestBody = [
+            'username' => $username,
+            'password' => $password,
+        ];
+
+        $client->request(Request::METHOD_POST, self::BASE_URL, server: [
+            'HTTP_ACCEPT' => 'application/json',
+            'CONTENT_TYPE' => 'application/json',
+        ], content: json_encode($requestBody));
+
+        self::assertResponseStatusCodeSame(Response::HTTP_OK);
+
+        $responseArray = self::getCurrentResponseArray();
+        self::assertArrayHasKey('token', $responseArray);
+
+        return $responseArray['token'];
+    }
+
+    public function testCanLogout(): void
+    {
+        $client = self::createClient();
+
+        $token = $this->loginAndGetToken($client, 'talysonsoares@example.com', UserFixtures::DEFAULT_PASSWORD);
+
+        $client->request(Request::METHOD_GET, self::AGENT_URL, server: [
+            'HTTP_AUTHORIZATION' => 'Bearer '.$token,
+            'HTTP_ACCEPT' => 'application/json',
+        ]);
+
+        $this->assertResponseIsSuccessful();
+
+        $client->request(Request::METHOD_POST, self::LOGOUT_URL, server: [
+            'HTTP_AUTHORIZATION' => 'Bearer '.$token,
+            'HTTP_ACCEPT' => 'application/json',
+        ]);
+
+        self::assertResponseStatusCodeSame(Response::HTTP_OK);
+
+        $client->request(Request::METHOD_GET, self::AGENT_URL, server: [
+            'HTTP_AUTHORIZATION' => 'Bearer '.$token,
+            'HTTP_ACCEPT' => 'application/json',
+        ]);
+
+        self::assertResponseStatusCodeSame(Response::HTTP_UNAUTHORIZED);
+        self::assertResponseBodySame([
+            'code' => 401,
+            'message' => 'Invalid JWT Token',
         ]);
     }
 }
