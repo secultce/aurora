6ed26600ff6b24ea584f6b3a3d26eaf9997c2493
TalysonSoares <talysonsoares40@gmail.com>
2024-10-24 16:04:00 -0300

    Create new admin page for list events

---
+++ cypress/e2e/admin/event/event-list.cy.js
@@ -0,0 +1,15 @@
+describe('Painel de Controle - Página de listar Eventos', () => {
+    beforeEach(() => {
+        cy.viewport(1920, 1080);
+        cy.visit('/painel/eventos');
+    });
+
+    it('Garante que a página de Eventos existe', () => {
+        cy.get('h2').contains('Meus Eventos').should('be.visible');
+    });
+
+    it('Garante que os eventos estejam visíveis ', () => {
+        cy.get('tbody > :nth-child(4) > :nth-child(1) > a').contains('Raízes do Sertão').should('be.visible');
+        cy.get('tbody > :nth-child(4) > :nth-child(2)').contains('11/08/2024 15:54:00').should('be.visible');
+    });
+})
