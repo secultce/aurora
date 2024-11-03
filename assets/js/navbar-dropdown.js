c879893635ef8ebbc292633d8cab7701ca66b07a
Sara Camilo <124682717+saracamilo@users.noreply.github.com>
2024-11-03 09:56:05 -0300

    create dropdown of navbar (#300)

---
+++ assets/js/navbar-dropdown.js
@@ -0,0 +1,40 @@
+document.addEventListener('DOMContentLoaded', () => {
+    function toggleDropdown() {
+        const dropdownMenu = document.getElementById("customDropdown");
+        dropdownMenu.classList.toggle("show");
+
+        setTimeout(() => {
+            document.getElementById("dropdownMenuButton").blur();
+        }, 100);
+    }
+
+    const dropdownButton = document.getElementById("dropdownMenuButton");
+    if (dropdownButton) {
+        dropdownButton.addEventListener('click', (e) => {
+            e.stopPropagation();
+            toggleDropdown();
+        });
+    }
+
+    document.addEventListener('click', (event) => {
+        const dropdownMenu = document.getElementById("customDropdown");
+
+        if (dropdownMenu && dropdownMenu.classList.contains('show')) {
+            if (!dropdownButton.contains(event.target) && !dropdownMenu.contains(event.target)) {
+                dropdownMenu.classList.remove('show');
+            }
+        }
+    });
+
+    const dropdownItems = document.querySelectorAll('.dropdown-item');
+    dropdownItems.forEach(item => {
+        item.addEventListener('click', () => {
+            const dropdownMenu = document.getElementById("customDropdown");
+            dropdownMenu.classList.remove('show');
+
+            setTimeout(() => {
+                dropdownButton.blur();
+            }, 100);
+        });
+    });
+});
