7b52290046be70596731b85617914aaf30004965
Sara Camilo <124682717+saracamilo@users.noreply.github.com>
2024-12-05 15:20:42 -0300

    create faq list (#388)

---
+++ cypress/e2e/admin/faq/faq-list.cy.js
@@ -0,0 +1,30 @@
+describe('Teste de navegação e validação da página de Minhas FAQs', () => {
+    it('Deve acessar a página de FaQ a partir de Oportunidades após login', () => {
+        cy.visit('/');
+
+        cy.contains('Entrar').click();
+
+        cy.url().should('include', '/login');
+
+        cy.login('saracamilo@example.com', 'Aurora@2024');
+
+        cy.url().should('include', '/');
+
+        cy.contains('Sara Jenifer Camilo').should('be.visible');
+        cy.contains('Sara Jenifer Camilo').click();
+
+        cy.contains('Minhas Oportunidades', { timeout: 10000 }).should('be.visible').click();
+
+        cy.url({ timeout: 10000 }).should('include', '/painel/oportunidades');
+
+        cy.scrollTo('bottom');
+
+        cy.contains('FaQ', { timeout: 10000 }).should('be.visible').click();
+
+        cy.url({ timeout: 10000 }).should('include', '/painel/faq/');
+
+        cy.get('table', { timeout: 10000 }).should('be.visible');
+
+        cy.get('table').contains('Como me inscrever', { timeout: 10000 }).should('be.visible');
+    });
+});
