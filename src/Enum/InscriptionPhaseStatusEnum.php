f2a0772ef317a693a6a818fc67b4a6b5df0af2fa
Henrique Lopes Lima <henriquelopeslima.hll@gmail.com>
2024-12-06 09:16:45 -0300

    Create the entity Inscription Phase

---
+++ src/Enum/InscriptionPhaseStatusEnum.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Enum;
+
+use App\Enum\Trait\EnumTrait;
+
+enum InscriptionPhaseStatusEnum: int
+{
+    use EnumTrait;
+
+    case ACTIVE = 1;
+    case INACTIVE = 2;
+    case SUSPENDED = 3;
+}
