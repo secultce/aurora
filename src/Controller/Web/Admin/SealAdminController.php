5ccf16ae19b28878b9c962395ccafcf152662409
Sara Camilo <124682717+saracamilo@users.noreply.github.com>
2024-12-09 10:43:39 -0300

    create seals list (#409)

--- src/Controller/Web/Admin/SealAdminController.php
+++ src/Controller/Web/Admin/SealAdminController.php
@@ -4,17 +4,33 @@ declare(strict_types=1);
 
 namespace App\Controller\Web\Admin;
 
+use DateTime;
 use Symfony\Component\HttpFoundation\Response;
 
 class SealAdminController extends AbstractAdminController
 {
     public function list(): Response
     {
-        return $this->render('seal/list.html.twig');
+        $seals = [
+            ['name' => 'Selo Um', 'status' => 'Ativo', 'createdAt' => new DateTime('2023-12-01 10:00:00')],
+            ['name' => 'Selo Dois', 'status' => 'Inativo', 'createdAt' => new DateTime('2023-11-25 15:30:00')],
+        ];
+
+        return $this->render('seal/list.html.twig', [
+            'seals' => $seals,
+        ]);
     }
 
     public function getOne(int $id): Response
     {
-        return $this->render('seal/one.html.twig');
+        $seal = [
+            'name' => 'Selo '.$id,
+            'status' => 'Ativo',
+            'createdAt' => new DateTime('2023-12-01 10:00:00'),
+        ];
+
+        return $this->render('seal/one.html.twig', [
+            'seal' => $seal,
+        ]);
     }
 }
