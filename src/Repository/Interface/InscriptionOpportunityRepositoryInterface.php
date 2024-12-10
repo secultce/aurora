c76b97b80e3b0ddb0531c612dba11c362744c9b2
igormendes <a.igor.mendes@gmail.com>
2024-12-10 09:18:08 -0300

    Automatically subscribe agent in first phase of opportunity

--- src/Repository/Interface/InscriptionOpportunityRepositoryInterface.php
+++ src/Repository/Interface/InscriptionOpportunityRepositoryInterface.php
@@ -4,8 +4,13 @@ declare(strict_types=1);
 
 namespace App\Repository\Interface;
 
+use App\Entity\InscriptionOpportunity;
+use App\Entity\InscriptionPhase;
+
 interface InscriptionOpportunityRepositoryInterface
 {
+    public function create(InscriptionOpportunity $inscriptionOpportunity, ?InscriptionPhase $inscriptionPhase = null): InscriptionOpportunity;
+
     public function findOneInscriptionOpportunity(string $inscriptionId, string $opportunityId, array $agents);
 
     public function findInscriptionsByOpportunity(string $opportunityId, array $agents, int $limit);
