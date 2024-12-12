094771c32fbf5bd866bc56cbdab4160543305115
Sara Camilo <saraccamilo@gmail.com>
2024-12-12 16:04:15 -0300

    create organization registration page

---
+++ cypress/e2e/admin/organization/organization-create.cy.js
@@ -0,0 +1,24 @@
+describe('Teste de navegação e validação da página de Organizações', () => {
+    it('Deve acessar a página de Minhas Organizações a partir de Oportunidades após login', () => {
+        cy.visit('/');
+
+        cy.contains('Entrar').click();
+
+        cy.url().should('include', '/login');
+
+        cy.login('saracamilo@example.com', 'Aurora@2024');
+
+        cy.url().should('include', '/');
+
+        cy.contains('Sara Jenifer Camilo').should('be.visible');
+        cy.contains('Sara Jenifer Camilo').click();
+        cy.contains('Minhas Oportunidades', { timeout: 10000 }).should('be.visible').click();
+        cy.url({ timeout: 10000 }).should('include', '/painel/oportunidades');
+        cy.scrollTo('bottom');
+        cy.contains('Minhas Organizações', { timeout: 10000 }).should('be.visible').click();
+        cy.url({ timeout: 10000 }).should('include', '/painel/organizacoes/');
+        cy.get('table', { timeout: 10000 }).should('be.visible');
+        cy.contains('Criar', { timeout: 10000 }).should('be.visible').click();
+        cy.url({ timeout: 10000 }).should('include', '/painel/organizacoes/adicionar');
+    });
+});
--- src/Controller/Web/Admin/OrganizationAdminController.php
+++ src/Controller/Web/Admin/OrganizationAdminController.php
@@ -4,13 +4,19 @@ declare(strict_types=1);
 
 namespace App\Controller\Web\Admin;
 
+use App\Exception\ValidatorException;
 use App\Service\Interface\OrganizationServiceInterface;
+use Exception;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Uid\Uuid;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 use Symfony\Contracts\Translation\TranslatorInterface;
 
 class OrganizationAdminController extends AbstractAdminController
 {
+    public const VIEW_ADD = 'organization/add.html.twig';
+
     public function __construct(
         private OrganizationServiceInterface $service,
         private readonly TranslatorInterface $translator,
@@ -24,6 +30,34 @@ class OrganizationAdminController extends AbstractAdminController
         return $this->render('organization/list.html.twig', [
             'organizations' => $organizations,
         ]);
+    }
+
+    public function add(Request $request, ValidatorInterface $validator): Response
+    {
+        if ('POST' !== $request->getMethod()) {
+            return $this->render(self::VIEW_ADD);
+        }
+
+        try {
+            $this->service->create([
+                'id' => Uuid::v4(),
+                'name' => $request->get('name'),
+            ]);
+        } catch (ValidatorException $exception) {
+            return $this->render(self::VIEW_ADD, [
+                'errors' => $exception->getConstraintViolationList(),
+            ]);
+        } catch (Exception $exception) {
+            return $this->render(self::VIEW_ADD, [
+                'errors' => [
+                    $exception->getMessage(),
+                ],
+            ]);
+        }
+
+        $this->addFlash('success', $this->translator->trans('view.organization.message.created'));
+
+        return $this->redirectToRoute('admin_organization_list');
     }
 
     public function remove(?Uuid $id): Response
