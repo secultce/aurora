3de365e6cac5588b5f54174d98866924065397eb
Alessandro Feitoza <alessandrofeitoza01@gmail.com>
2024-12-13 12:24:24 -0300

    Adjust faq page for create just a modal (#428)

--- cypress/e2e/web/faq/faq-card.cy.js
+++ cypress/e2e/web/faq/faq-card.cy.js
@@ -3,19 +3,19 @@ describe('Teste de acesso à FAQ e interação com os cards', () => {
         cy.visit('/faq');
     });
 
-    it('Deve abrir os modais ao clicar nos ícones dos cards', () => {
+    it('Deve abrir os modais ao clicar nos cards', () => {
         cy.scrollTo('bottom');
 
         cy.get('.card-faq-item')
             .should('be.visible')
             .each(($card) => {
-                const modalTarget = $card.find('i').attr('data-bs-target');
-                cy.wrap($card).find('i').click();
+                const modalTarget = $card;
+                cy.wrap($card).click();
                 cy.wait(500);
                 cy.get(modalTarget)
                     .should('be.visible');
-                cy.get(`${modalTarget} .btn-close`).click();
-                cy.get(modalTarget).should('not.be.visible');
+                cy.get('#faqModalAnswer button.btn-close').click();
+                cy.get('#faqModalAnswer button.btn-close').should('not.be.visible');
             });
     });
 });
