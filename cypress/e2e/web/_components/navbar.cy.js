9f8af1241051411325fa26459a6eed4ad6b9b3ab
Talyson Soares <talysonsoares40@gmail.com>
2024-12-13 11:36:26 -0300

    Create organization page (#430)

--- cypress/e2e/web/_components/navbar.cy.js
+++ cypress/e2e/web/_components/navbar.cy.js
@@ -8,7 +8,8 @@ describe('Página de Home do ambiente web', () => {
         { name: 'Oportunidades', path: '/web_opportunity_list' },
         { name: 'Eventos', path: '/web_event_list' },
         { name: 'Espaços', path: '/web_space_list' },
-        { name: 'Agentes', path: '/admin_agent_list' },
+        { name: 'Agentes', path: '/web_agent_list' },
+        { name: 'Organizações', path: '/web_organization_list' },
         { name: 'Iniciativas', path: '/admin_initiative_list' }
     ];
 
@@ -27,6 +28,11 @@ describe('Página de Home do ambiente web', () => {
     it('Clica no ícone de Agentes e garante o redirecionamento', () => {
         cy.get('a.nav-link.agent').should('be.visible').and('not.be.disabled').click({ force: true });
         cy.url().should('include', '/agentes');
+    });
+
+    it('Clica no ícone de Organizações e garante o redirecionamento', () => {
+        cy.get('a.nav-link.organization').should('be.visible').and('not.be.disabled').click({ force: true });
+        cy.url().should('include', '/organizacoes');
     });
 
     it('Clica no ícone de Eventos e garante o redirecionamento', () => {
---
+++ src/Controller/Web/OrganizationWebController.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller\Web;
+
+use App\Service\Interface\OrganizationServiceInterface;
+use App\ValueObject\DashboardCardItemValueObject as CardItem;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class OrganizationWebController extends AbstractWebController
+{
+    public function __construct(
+        public readonly OrganizationServiceInterface $service,
+    ) {
+    }
+
+    public function list(Request $request): Response
+    {
+        $organizations = $this->service->list();
+
+        $totalOrganizations = count($organizations);
+
+        $dashboard = [
+            'color' => '#6A5ACD',
+            'items' => [
+                new CardItem(icon: 'description', quantity: $totalOrganizations, text: 'view.organization.quantity.total'),
+                new CardItem(icon: 'person', quantity: 30, text: 'view.organization.quantity.culture'),
+                new CardItem(icon: 'block', quantity: 20, text: 'view.organization.quantity.inactive'),
+                new CardItem(icon: 'today', quantity: 10, text: 'view.organization.quantity.last_days'),
+            ],
+        ];
+
+        return $this->render('organization/list.html.twig', [
+            'dashboard' => $dashboard,
+            'organizations' => $organizations,
+            'totalOrganizations' => $totalOrganizations,
+        ]);
+    }
+}
