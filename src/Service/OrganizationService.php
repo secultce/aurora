5b234e3e3bc0aafe6eee3963fd1f82d64c47d0cf
TalysonSoares <talysonsoares40@gmail.com>
2024-12-13 12:25:48 -0300

    Adds side filter to organization page

--- src/Service/OrganizationService.php
+++ src/Service/OrganizationService.php
@@ -95,10 +95,10 @@ readonly class OrganizationService extends AbstractEntityService implements Orga
         return $organization;
     }
 
-    public function list(int $limit = 50): array
+    public function list(int $limit = 50, array $params = []): array
     {
         return $this->repository->findBy(
-            $this->getDefaultParams(),
+            [...$params, ...$this->getDefaultParams()],
             ['createdAt' => 'DESC'],
             $limit
         );
