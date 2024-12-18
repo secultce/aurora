5481e75472a0180caf93b0d0f3588ae1a716d68c
Akellymourab <anakellymourab@gmail.com>
2024-12-17 22:43:49 -0300

    Generate internal token for API consumption via Twig.

--- src/Controller/Web/Admin/AgentAdminController.php
+++ src/Controller/Web/Admin/AgentAdminController.php
@@ -5,22 +5,28 @@ declare(strict_types=1);
 namespace App\Controller\Web\Admin;
 
 use App\Service\Interface\AgentServiceInterface;
+use Lexik\Bundle\JWTAuthenticationBundle\Services\JWTTokenManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Uid\Uuid;
 
 class AgentAdminController extends AbstractAdminController
 {
     public function __construct(
-        private AgentServiceInterface $service
+        private AgentServiceInterface $service,
+        private JWTTokenManagerInterface $jwtManager
     ) {
     }
 
-    public function list(): Response
+    public function list(UserInterface $user): Response
     {
         $agents = $this->service->findBy();
 
+        $token = $this->jwtManager->create($user);
+
         return $this->render('agent/list.html.twig', [
             'agents' => $agents,
+            'token' => $token,
         ]);
     }
 
