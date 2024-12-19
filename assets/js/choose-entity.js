e413ebcd787d88c8ac05fd882591c0d7774d5c9c
Ronny John <me@ronnyjohnti.dev>
2024-12-19 17:08:59 -0300

    feat: Create registration opportunity page

---
+++ assets/js/choose-entity.js
@@ -0,0 +1,24 @@
+function searchEntity(target) {
+    const value = target.value.toLowerCase();
+    for (const el of target.parentNode.parentNode.parentNode.querySelectorAll(".list-group li")) {
+        if (el.textContent.toLowerCase().includes(value)) {
+            el.classList.remove('d-none');
+            el.classList.add('list-group-item');
+        } else {
+            el.classList.remove('list-group-item');
+            el.classList.add('d-none');
+        }
+    }
+}
+
+function selectEntity(target) {
+    const entityId = target.getAttribute('data-entity-id');
+    const entityType = target.getAttribute('data-entity-type');
+    const fieldName = target.getAttribute('data-field-name');
+
+    document.querySelectorAll(`input[name="${fieldName}"]`).forEach(el => el.nextElementSibling.textContent = '');
+
+    const entityInput = document.querySelector(`input[name="${fieldName}"][value^="${entityType}_"]`);
+    entityInput.value = entityType + '_' + entityId;
+    entityInput.nextElementSibling.textContent = ': ' + target.textContent;
+}
