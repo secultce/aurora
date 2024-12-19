f586e38274b70802275cae089483e7c8ada1182d
Ronny John <me@ronnyjohnti.dev>
2024-12-19 17:08:59 -0300

    feat: Create registration opportunity page

--- src/Controller/Web/Admin/OpportunityAdminController.php
+++ src/Controller/Web/Admin/OpportunityAdminController.php
@@ -6,13 +6,27 @@ namespace App\Controller\Web\Admin;
 
 use App\Service\Interface\OpportunityServiceInterface;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Uid\Uuid;
+use Symfony\Component\Validator\ConstraintViolationList;
+use Symfony\Contracts\Translation\TranslatorInterface;
 
 class OpportunityAdminController extends AbstractAdminController
 {
     public function __construct(
         private OpportunityServiceInterface $service,
+        private readonly TranslatorInterface $translator,
     ) {
+    }
+
+    public function create(): Response
+    {
+        $opportunities = $this->service->findBy();
+
+        return $this->render('opportunity/create.html.twig', [
+            'id' => Uuid::v4()->toRfc4122(),
+            'opportunities' => $opportunities,
+        ]);
     }
 
     public function list(): Response
@@ -29,6 +43,17 @@ class OpportunityAdminController extends AbstractAdminController
         $this->service->remove($id);
 
         $this->addFlash('success', 'view.opportunity.message.deleted');
+
+        return $this->redirectToRoute('admin_opportunity_list');
+    }
+
+    public function store(Request $request): Response
+    {
+        $data = $request->request->all();
+
+        $this->service->create($data);
+
+        $this->addFlash('success', $this->translator->trans('view.opportunity.message.created'));
 
         return $this->redirectToRoute('admin_opportunity_list');
     }
