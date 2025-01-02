9563660e1d5991b075f19227e44a249e53627d1d
Henrique Lopes Lima <henriquelopeslima.hll@gmail.com>
2025-01-02 16:49:07 -0300

    Create the endpoint to create and update the Seals

--- src/Controller/Api/SealApiController.php
+++ src/Controller/Api/SealApiController.php
@@ -7,6 +7,7 @@ namespace App\Controller\Api;
 use App\Helper\EntityIdNormalizerHelper;
 use App\Service\Interface\SealServiceInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
 use Symfony\Component\Uid\Uuid;
@@ -16,6 +17,13 @@ class SealApiController extends AbstractApiController
     public function __construct(
         private readonly SealServiceInterface $service,
     ) {
+    }
+
+    public function create(Request $request): JsonResponse
+    {
+        $seal = $this->service->create($request->toArray());
+
+        return $this->json($seal, status: Response::HTTP_CREATED, context: ['groups' => ['seal.get', 'seal.get.item']]);
     }
 
     public function get(?Uuid $id): JsonResponse
@@ -40,5 +48,12 @@ class SealApiController extends AbstractApiController
         $this->service->remove($id);
 
         return $this->json(data: [], status: Response::HTTP_NO_CONTENT);
+    }
+
+    public function update(?Uuid $id, Request $request): JsonResponse
+    {
+        $seal = $this->service->update($id, $request->toArray());
+
+        return $this->json($seal, Response::HTTP_OK, context: ['groups' => ['seal.get', 'seal.get.item']]);
     }
 }
--- src/Service/SealService.php
+++ src/Service/SealService.php
@@ -4,21 +4,36 @@ declare(strict_types=1);
 
 namespace App\Service;
 
+use App\DTO\SealDto;
 use App\Entity\Seal;
 use App\Exception\Seal\SealResourceNotFoundException;
+use App\Exception\ValidatorException;
 use App\Repository\Interface\SealRepositoryInterface;
 use App\Service\Interface\SealServiceInterface;
 use DateTime;
 use Symfony\Bundle\SecurityBundle\Security;
+use Symfony\Component\Serializer\SerializerInterface;
 use Symfony\Component\Uid\Uuid;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 readonly class SealService extends AbstractEntityService implements SealServiceInterface
 {
     public function __construct(
         private SealRepositoryInterface $repository,
         private Security $security,
+        private SerializerInterface $serializer,
+        private ValidatorInterface $validator,
     ) {
         parent::__construct($this->security);
+    }
+
+    public function create(array $seal): Seal
+    {
+        $seal = self::validateInput($seal, SealDto::CREATE);
+
+        $sealObj = $this->serializer->denormalize($seal, Seal::class);
+
+        return $this->repository->save($sealObj);
     }
 
     public function findBy(array $params = [], int $limit = 50): array
@@ -73,5 +88,33 @@ readonly class SealService extends AbstractEntityService implements SealServiceI
         $seal->setDeletedAt(new DateTime());
 
         $this->repository->save($seal);
+    }
+
+    public function update(Uuid $identifier, array $seal): Seal
+    {
+        $sealFromDB = $this->get($identifier);
+
+        $sealDto = self::validateInput($seal, SealDto::UPDATE);
+
+        $sealObj = $this->serializer->denormalize($sealDto, Seal::class, context: [
+            'object_to_populate' => $sealFromDB,
+        ]);
+
+        $sealObj->setUpdatedAt(new DateTime());
+
+        return $this->repository->save($sealObj);
+    }
+
+    private function validateInput(array $seal, string $group): array
+    {
+        $sealDto = $this->serializer->denormalize($seal, SealDto::class);
+
+        $violations = $this->validator->validate($sealDto, groups: $group);
+
+        if ($violations->count() > 0) {
+            throw new ValidatorException(violations: $violations);
+        }
+
+        return $seal;
     }
 }
