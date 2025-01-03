e1db2f44c47f8e1a9235cd61578ea733847d4d0e
Sara Camilo <saraccamilo@gmail.com>
2025-01-02 22:57:10 -0300

    create timeline of events

---
+++ cypress/e2e/admin/event/event-timeline.cy.js
@@ -0,0 +1,21 @@
+describe('Painel de Controle - Página de listar eventos', () => {
+    beforeEach(() => {
+        cy.viewport(1920, 1080);
+        cy.login('alessandrofeitoza@example.com', 'Aurora@2024');
+        cy.visit('painel/eventos');
+    });
+
+    it('Garante que a página de timeline de evento existe', () => {
+        cy.contains('Timeline').click({force: true});
+
+        cy.wait(500);
+
+        cy.get('h2').contains('Evento - PHP com Rapadura 10 anos - Timeline').should('be.visible');
+        cy.get('.d-flex > div > .btn').contains('Voltar').should('be.visible');
+
+        cy.get('tr > :nth-child(1) > a').contains('The resource was created').should('be.visible');
+        cy.get('tbody > tr > :nth-child(2)').contains(/\d{2}\/\d{2}\/\d{4}/).should('be.visible');
+        cy.get('tbody > tr > :nth-child(3)').contains('unknown').should('be.visible');
+        cy.get(':nth-child(5) > .btn').contains('Detalhes').should('be.visible');
+    });
+});
