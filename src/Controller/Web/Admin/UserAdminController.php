29358a59f6977514627aaef72f8a1a69d9281468
TalysonSoares <talysonsoares40@gmail.com>
2025-01-03 11:37:25 -0300

    Create page to display user timeline

--- src/Controller/Web/Admin/UserAdminController.php
+++ src/Controller/Web/Admin/UserAdminController.php
@@ -4,13 +4,16 @@ declare(strict_types=1);
 
 namespace App\Controller\Web\Admin;
 
+use App\DocumentService\UserTimelineDocumentService;
 use App\Service\Interface\UserServiceInterface;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Uid\Uuid;
 
 class UserAdminController extends AbstractAdminController
 {
     public function __construct(
-        private UserServiceInterface $service
+        private readonly UserTimelineDocumentService $documentService,
+        private readonly UserServiceInterface $service,
     ) {
     }
 
@@ -20,6 +23,16 @@ class UserAdminController extends AbstractAdminController
 
         return $this->render('user/list.html.twig', [
             'users' => $users,
+        ]);
+    }
+
+    public function timeline(Uuid $id): Response
+    {
+        $events = $this->documentService->getEventsByEntityId($id);
+
+        return $this->render('user/timeline.html.twig', [
+            'user' => $this->service->get($id),
+            'events' => $events,
         ]);
     }
 }
