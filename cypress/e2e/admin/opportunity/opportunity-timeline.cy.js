372975aa94c41f0c7029cc8e8bebc34c36711ebb
Talyson Soares <talysonsoares40@gmail.com>
2025-01-04 16:20:54 -0300

    Create page to display opportunity timeline (#462)

--- cypress/e2e/admin/opportunity/opportunity-timeline.cy.js
+++ cypress/e2e/admin/opportunity/opportunity-timeline.cy.js
@@ -1,16 +1,16 @@
-describe('Painel de Controle - Página de timeline das Iniciativas', () => {
+describe('Painel de Controle - Página de timeline das Oportunidades', () => {
     beforeEach(() => {
         cy.viewport(1920, 1080);
-        cy.login('alessandrofeitoza@example.com', 'Aurora@2024');
-        cy.visit('painel/iniciativas/f0774ecd-4860-4b8c-9607-32090dc31f71/timeline');
+        cy.login('henriquelopeslima@example.com', 'Aurora@2024');
     });
 
     it('Garante que a página de timeline existe', () => {
-        cy.get('h2').contains('Iniciativa - Vozes do Sertão - Timeline').should('be.visible');
-        cy.get('.d-flex > div > .btn').contains('Voltar').should('be.visible');
-    });
+        cy.visit('painel/oportunidades/');
 
-    it('Garante que os dados da iniciativa estão visíveis', () => {
+        cy.contains('Timeline').click({force: true});
+
+        cy.get('h2').contains('Oportunidade - Chamada para Oficinas de Artesanato - Feira de Cultura Popular - Timeline').should('be.visible');
+        cy.get('.d-flex > div > .btn').contains('Voltar').should('be.visible');
         cy.get('tr > :nth-child(1) > a').contains('The resource was created').should('be.visible');
         cy.get('tbody > tr > :nth-child(2)').contains(/\d{2}\/\d{2}\/\d{4}/).should('be.visible');
         cy.get('tbody > tr > :nth-child(3)').contains('unknown').should('be.visible');
