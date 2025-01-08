25d2282cc241f6d3ba5adda32bc0c6780c455d55
Sara Camilo <124682717+saracamilo@users.noreply.github.com>
2025-01-08 11:33:35 -0300

    create edit seals page (#475)

--- cypress/e2e/admin/seal/seal-create.cy.js
+++ cypress/e2e/admin/seal/seal-create.cy.js
@@ -3,34 +3,36 @@ describe('Teste de navegação e validação da página de Selos', () => {
         cy.visit('/');
 
         cy.contains('Entrar').click();
-
         cy.url().should('include', '/login');
-
         cy.login('saracamilo@example.com', 'Aurora@2024');
-
         cy.url().should('include', '/');
-
         cy.contains('Sara Jenifer Camilo').should('be.visible');
+
         cy.contains('Sara Jenifer Camilo').click();
-        cy.contains('Minhas Oportunidades', { timeout: 10000 }).should('be.visible').click();
+        cy.contains('Minhas Oportunidades', { timeout: 10000 })
+            .should('be.visible')
+            .click();
         cy.url({ timeout: 10000 }).should('include', '/painel/oportunidades');
         cy.scrollTo('bottom');
-        cy.contains('Selos', { timeout: 10000 }).should('be.visible').click();
+        cy.contains('Selos', { timeout: 10000 })
+            .should('be.visible')
+            .click();
         cy.url({ timeout: 10000 }).should('include', '/painel/selos/');
         cy.get('table', { timeout: 10000 }).should('be.visible');
-        cy.contains('Criar', { timeout: 10000 }).should('be.visible').click();
+
+        cy.contains('Criar', { timeout: 10000 })
+            .should('be.visible')
+            .click();
         cy.url({ timeout: 10000 }).should('include', '/painel/selos/adicionar');
 
-        // Garante que a validação existe
         cy.get('button').contains('Salvar').should('be.visible').click();
         cy.get('input:invalid').should('have.length', 1);
         cy.get('textarea:invalid').should('have.length', 1);
 
-        // Garante que a criação funciona
         cy.get('input[name="name"]').type('Selo de qualidade');
         cy.get('textarea[name="description"]').type('Descrição do selo');
         cy.get('button').contains('Salvar').should('be.visible').click();
+
         cy.url({ timeout: 10000 }).should('include', '/painel/selos/');
-        cy.contains('Selo de qualidade').should('be.visible');
     });
 });
---
+++ cypress/e2e/admin/seal/seal-edit.cy.js
@@ -0,0 +1,57 @@
+describe('Teste de navegação, validação e edição da página de Selos', () => {
+    beforeEach(() => {
+        cy.visit('/');
+        cy.contains('Entrar').click();
+        cy.url().should('include', '/login');
+        cy.login('saracamilo@example.com', 'Aurora@2024');
+        cy.url().should('include', '/');
+        cy.contains('Sara Jenifer Camilo').should('be.visible').click();
+        cy.contains('Minhas Oportunidades').should('be.visible').click();
+        cy.url().should('include', '/painel/oportunidades');
+        cy.scrollTo('bottom');
+        cy.contains('Selos').should('be.visible').click();
+        cy.url().should('include', '/painel/selos/');
+        cy.contains('Editar').first().click();
+        cy.url().should('include', '/editar');
+
+        cy.get('form').invoke('attr', 'novalidate', true);
+    });
+
+    it('Verifica e edita os campos do formulário de selos', () => {
+        cy.get('input[name="active"]')
+            .should('exist')
+            .should('have.attr', 'type', 'checkbox')
+            .should('be.checked');
+
+        cy.get('input[name="name"]').clear().type('Selo Teste Atualizado');
+
+        cy.get('textarea[name="description"]')
+            .clear()
+            .type('Selo que destaca eventos com impacto em comunidades locais.');
+
+        cy.contains('Salvar').click();
+        cy.url().should('include', '/painel/selos/');
+        cy.contains('Selo Teste Atualizado').should('be.visible');
+        cy.contains('Selo Teste Atualizado').parent().contains('Editar').click();
+        cy.get('input[name="name"]').should('have.value', 'Selo Teste Atualizado');
+        cy.get('textarea[name="description"]').should(
+            'have.value',
+            'Selo que destaca eventos com impacto em comunidades locais.'
+        );
+        cy.get('input[name="active"]').should('be.checked');
+    });
+
+    it('Verifica os botões Salvar e Cancelar', () => {
+        cy.contains('Salvar')
+            .should('exist')
+            .should('have.attr', 'type', 'submit')
+
+        cy.contains('Cancelar')
+            .should('exist')
+            .should('have.attr', 'href', '/painel/selos/')
+            .should('be.visible');
+
+        cy.contains('Cancelar').click();
+        cy.url().should('include', '/painel/selos/');
+    });
+});
