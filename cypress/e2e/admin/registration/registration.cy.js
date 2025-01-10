cb23a4f609252f3967b9c35be1ee9727cf0be833
TalysonSoares <talysonsoares40@gmail.com>
2025-01-10 14:55:08 -0300

    Implement backend on registration page

--- cypress/e2e/admin/registration/registration.cy.js
+++ cypress/e2e/admin/registration/registration.cy.js
@@ -10,5 +10,9 @@ describe('Página de Minhas Inscrições', () => {
         cy.get('h1').contains('Minhas Inscrições');
         cy.get('input[placeholder="Busque por palavras-chave"]').should('exist');
         cy.get('button').contains('Acompanhar').should('exist');
+        cy.get(':nth-child(3) > .resource-card-header > .resource-title').contains("Inscrição para o Concurso de Cordelistas - Festival de Literatura Nordestina").should("be.visible");
+        cy.get(':nth-child(3) > .resource-card-body > .resource-details > :nth-child(1)').contains("Fase de recurso").should("be.visible");
+        cy.get(':nth-child(3) > .resource-card-body > .resource-details > :nth-child(2)').contains("Fase de recurso do Concurso de Cordelistas").should("be.visible");
+        cy.get(':nth-child(3) > .resource-card-body > .resource-details > :nth-child(3)').contains("23/07/2024 até 26/07/2024").should("be.visible");
     });
 });
