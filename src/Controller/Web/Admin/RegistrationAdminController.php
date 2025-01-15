344cd6ca7e92caf7505da7f9b3328ef7ba6e2327
Talyson Soares <talysonsoares40@gmail.com>
2025-01-15 11:25:24 -0300

    Implement backend on registration page (#512)

--- src/Controller/Web/Admin/RegistrationAdminController.php
+++ src/Controller/Web/Admin/RegistrationAdminController.php
@@ -4,12 +4,22 @@ declare(strict_types=1);
 
 namespace App\Controller\Web\Admin;
 
+use App\Service\Interface\InscriptionOpportunityServiceInterface;
 use Symfony\Component\HttpFoundation\Response;
 
 class RegistrationAdminController extends AbstractAdminController
 {
+    public function __construct(
+        private readonly InscriptionOpportunityServiceInterface $service,
+    ) {
+    }
+
     public function list(): Response
     {
-        return $this->render('registration/list.html.twig');
+        $inscriptions = $this->service->findUserInscriptionsWithDetails();
+
+        return $this->render('registration/list.html.twig', [
+            'inscriptions' => $inscriptions,
+        ]);
     }
 }
