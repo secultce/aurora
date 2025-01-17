e2502d5aff80befde5dfc091e85322439489be9e
Sara Camilo <saraccamilo@gmail.com>
2025-01-17 15:06:42 -0300

    create details user page

--- cypress/e2e/admin/user/user-timeline.cy.js
+++ cypress/e2e/admin/user/user-timeline.cy.js
@@ -5,13 +5,19 @@ describe('Painel de Controle - Página de timeline dos Usuários', () => {
         cy.visit('/painel/admin/usuarios/4ed47ceb-8415-4710-a072-3de52843c616/timeline');
     });
 
-    it('Garante que a página de timeline dos usuários existe', () => {
+    it('Garante que a página de timeline dos usuários existe e que exibe os detalhes corretamente', () => {
+
         cy.get('h2').contains('Usuário - Talyson Soares - Timeline').should('be.visible');
         cy.get('.d-flex > div > .btn').contains('Voltar').should('be.visible');
 
         cy.get('tr > :nth-child(1) > a').contains('The resource was created').should('be.visible');
         cy.get('tbody > tr > :nth-child(2)').contains(/\d{2}\/\d{2}\/\d{4}/).should('be.visible');
         cy.get('tbody > tr > :nth-child(3)').contains('unknown').should('be.visible');
-        cy.get(':nth-child(5) > .btn').contains('Detalhes').should('be.visible')
+        cy.get(':nth-child(5) > .btn').contains('Detalhes').should('be.visible');
+        cy.get(':nth-child(1) > :nth-child(5) > .btn').click();
+        cy.get('.modal-body > .table > thead > tr > :nth-child(2)').contains('De');
+        cy.get('.modal-body > .table > thead > tr > :nth-child(3)').contains('Para');
+        cy.get('#modal-timeline-table-body > :nth-child(2) > :nth-child(2)').contains('N/A');
+        cy.get('#modal-timeline-table-body > :nth-child(2) > :nth-child(3)').contains('Talyson');
     });
 });
