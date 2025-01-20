774c6c690a5b017a4c3eac790d620d39962bb3b1
Jaime Aguiar <jaimegaguiar@gmail.com>
2025-01-20 17:08:58 -0300

    feature/create-edit-event-page

--- src/Controller/Web/Admin/EventAdminController.php
+++ src/Controller/Web/Admin/EventAdminController.php
@@ -6,7 +6,9 @@ namespace App\Controller\Web\Admin;
 
 use App\Document\EventTimeline;
 use App\DocumentService\EventTimelineDocumentService;
+use App\Exception\ValidatorException;
 use App\Service\Interface\EventServiceInterface;
+use PHPUnit\Exception;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Uid\Uuid;
@@ -16,6 +18,7 @@ use TypeError;
 class EventAdminController extends AbstractAdminController
 {
     private const VIEW_ADD = 'event/create.html.twig';
+    private const VIEW_EDIT = 'event/edit.html.twig';
 
     public function __construct(
         private EventServiceInterface $service,
@@ -88,5 +91,69 @@ class EventAdminController extends AbstractAdminController
         }
 
         return $this->list();
+    }
+
+    public function edit(?Uuid $id, Request $request): Response
+    {
+        $event = $this->service->get($id);
+        return $this->render(self::VIEW_EDIT, [
+            'event' => $event,
+        ]);
+    }
+    public function update(Request $request, Uuid $id): Response
+    {
+        //dd($request->request->all());
+        $data = $request->request->all();
+//        dd($data['event-name']);
+        $event = $this->service->get($id);
+
+//        $image = $data['cover-image'];
+        $name = $data['event-name'];
+        $subtitle = ['event-subtitle'];
+        $shortDescription = $data['short-description'];
+        $longDescription = $data['long-description'];
+        $siteUrl = $data['site-url'];
+        $linkDescription = $data['link-description'];
+        $ageRating = $data['age-rating'];
+//        $culturalLanguage = $data['culturalArea'];
+
+        $dataToUpdate = [
+            'name' => $name,
+            'extraFields' => [
+              'subtitle' => $subtitle,
+              'short_description' => $shortDescription,
+              'long_description' => $longDescription,
+              'site_url' => $siteUrl,
+              'link_description' => $linkDescription,
+              'age_rating' => $ageRating,
+//              'cultural_language' => $culturalLanguage,
+            ],
+        ];
+
+        try {
+            $this->service->update($id, $dataToUpdate);
+
+            $this->addFlashSuccess('success', $this->translator->trans('view.event.message.updated'));
+        } catch (ValidatorException $exception) {
+            $this->addFlashError('error', $this->translator->trans('view.event.message.updated'));
+
+            return $this->render(self::VIEW_EDIT, [
+                'event' => $event,
+                'error' => $exception->getMessage(),
+            ]);
+        }
+
+        catch (Exception $exception) {
+            $this->addFlashError('error', $this->translator->trans('view.entities.message.required_fields'));
+
+            $events = $this->render('_admin/event/edit.html.twig');
+
+            return $this->render('_admin/event/edit.html.twig', [
+                'id' => Uuid::v4(),
+                'events' => $events,
+            ]);
+        }
+
+        return $this->redirectToRoute('admin_event_list');
     }
 }
