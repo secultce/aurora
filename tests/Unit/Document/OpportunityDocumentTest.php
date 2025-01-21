777226356deed8b97c26975f9c5d3804d5ff4f42
jgaguiarm <jaimegaguiar@gmail.com>
2025-01-21 20:53:59 -0300

    test/opportunity-timeline-test

---
+++ tests/Unit/Document/OpportunityDocumentTest.php
@@ -0,0 +1,71 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Unit\Document;
+
+use App\Document\OpportunityTimeline;
+use App\Tests\AbstractWebTestCase;
+use DateTime;
+
+class OpportunityDocumentTest extends AbstractWebTestCase
+{
+    public function testsGettersAndSettersFromOpportunityShouldBeSuccessful(): void
+    {
+        $opportunity = new OpportunityTimeline();
+
+        $this->assertNull($opportunity->getId());
+
+        $id = 'fbeb1195-7040-4600-877b-492d40de7352';
+        $userId = 'fbeb1195-7040-4600-877b-492d40de7310';
+        $resourceId = 'abcb1195-7040-4600-877b-492d40de7352';
+        $title = 'My Opportunity';
+        $priority = 10;
+        $datetime = new DateTime();
+        $device = 'Smartphone';
+        $platform = 'Android';
+        $from = ['string1', 1];
+        $to = ['to', 55];
+
+        $opportunity->setId($id);
+        $opportunity->setUserId($userId);
+        $opportunity->setResourceId($resourceId);
+        $opportunity->setTitle($title);
+        $opportunity->setPriority($priority);
+        $opportunity->setDatetime($datetime);
+        $opportunity->setDevice($device);
+        $opportunity->setPlatform($platform);
+        $opportunity->setFrom($from);
+        $opportunity->setTo($to);
+
+        $this->assertEquals($id, $opportunity->getId());
+        $this->assertIsString($opportunity->getId());
+
+        $this->assertEquals($userId, $opportunity->getUserId());
+        $this->assertIsString($opportunity->getUserId());
+
+        $this->assertEquals($resourceId, $opportunity->getResourceId());
+        $this->assertIsString($opportunity->getResourceId());
+
+        $this->assertEquals($title, $opportunity->getTitle());
+        $this->assertIsString($opportunity->getTitle());
+
+        $this->assertEquals($priority, $opportunity->getPriority());
+        $this->assertIsInt($opportunity->getPriority());
+
+        $this->assertEquals($datetime, $opportunity->getDatetime());
+        $this->assertInstanceOf(DateTime::class, $opportunity->getDatetime());
+
+        $this->assertEquals($device, $opportunity->getDevice());
+        $this->assertIsString($opportunity->getDevice());
+
+        $this->assertEquals($platform, $opportunity->getPlatform());
+        $this->assertIsString($opportunity->getPlatform());
+
+        $this->assertEquals($from, $opportunity->getFrom());
+        $this->assertIsArray($opportunity->getFrom());
+
+        $this->assertEquals($to, $opportunity->getTo());
+        $this->assertIsArray($opportunity->getTo());
+    }
+}
