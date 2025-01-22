5b62b21cbb7993797407ea3d2cf7b063710502eb
Henrique Lopes Lima <henriquelopeslima.hll@gmail.com>
2025-01-21 21:10:40 -0300

    Fix flow inscription fixtures

--- migrations/Version20250120220338.php
+++ migrations/Version20250120220338.php
@@ -18,8 +18,8 @@ final class Version20250120220338 extends AbstractMigration
     {
         $this->addSql('CREATE TABLE inscription_phase_review (id UUID NOT NULL, inscription_phase_id UUID NOT NULL, reviewer_id UUID NOT NULL, result JSON DEFAULT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, updated_at TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL, deleted_at TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL, PRIMARY KEY(id))');
 
-        $this->addSql('CREATE UNIQUE INDEX UNIQ_41B01415309C6E2D ON inscription_phase_review (inscription_phase_id)');
-        $this->addSql('CREATE UNIQUE INDEX UNIQ_41B0141570574616 ON inscription_phase_review (reviewer_id)');
+        $this->addSql('CREATE INDEX UNIQ_41B01415309C6E2D ON inscription_phase_review (inscription_phase_id)');
+        $this->addSql('CREATE INDEX UNIQ_41B0141570574616 ON inscription_phase_review (reviewer_id)');
 
         $this->addSql('COMMENT ON COLUMN inscription_phase_review.id IS \'(DC2Type:uuid)\'');
         $this->addSql('COMMENT ON COLUMN inscription_phase_review.inscription_phase_id IS \'(DC2Type:uuid)\'');
