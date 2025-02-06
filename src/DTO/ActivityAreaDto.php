2c843c07c6a3c2b7662138573b114446714ed343
Ana Kelly Moura Balbino <101601082+Akellymourab@users.noreply.github.com>
2025-02-06 11:48:14 -0300

    Adds endpoints for create and update ActivityArea. (#15)

---
+++ src/DTO/ActivityAreaDto.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\DTO;
+
+use App\Validator\Constraints\NotNull;
+use Doctrine\DBAL\Types\Types;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Sequentially;
+use Symfony\Component\Validator\Constraints\Type;
+use Symfony\Component\Validator\Constraints\Uuid;
+
+class ActivityAreaDto
+{
+    public const string CREATE = 'create';
+
+    public const string UPDATE = 'update';
+
+    #[Sequentially([new NotBlank(), new Uuid()], groups: [self::CREATE])]
+    public mixed $id;
+
+    #[Sequentially([
+        new NotBlank(),
+        new NotNull(),
+        new Type(Types::STRING),
+        new Length(min: 2, max: 20),
+    ], groups: [self::CREATE, self::UPDATE])]
+    public mixed $name;
+}
--- src/Service/Interface/ActivityAreaServiceInterface.php
+++ src/Service/Interface/ActivityAreaServiceInterface.php
@@ -9,6 +9,10 @@ use Symfony\Component\Uid\Uuid;
 
 interface ActivityAreaServiceInterface
 {
+    public function create(array $activityArea): ActivityArea;
+
+    public function update(Uuid $id, array $activityArea): ActivityArea;
+
     public function get(Uuid $id): ActivityArea;
 
     public function list(int $limit = 50): array;
