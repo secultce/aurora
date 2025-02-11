8a0991ce683060efba2a7d34bd1fca19e3cc7676
Talyson Soares <talysonsoares40@gmail.com>
2025-02-11 13:30:44 -0300

    Adds Create Tag Page (#32)

--- src/Controller/Web/Admin/TagAdminController.php
+++ src/Controller/Web/Admin/TagAdminController.php
@@ -4,20 +4,58 @@ declare(strict_types=1);
 
 namespace App\Controller\Web\Admin;
 
+use App\Enum\FlashMessageTypeEnum;
+use App\Exception\ValidatorException;
 use App\Service\TagService;
+use Exception;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Uid\Uuid;
+use Symfony\Contracts\Translation\TranslatorInterface;
 
 class TagAdminController extends AbstractAdminController
 {
+    private const string ADD = 'tag/create.html.twig';
+    private const string LIST = 'tag/list.html.twig';
+
     public function __construct(
         private readonly TagService $tagService,
+        private readonly TranslatorInterface $translator,
     ) {
     }
 
     public function list(): Response
     {
-        return $this->render('/tag/list.html.twig', [
+        return $this->render(self::LIST, [
             'tags' => $this->tagService->list(),
         ]);
+    }
+
+    public function create(Request $request): Response
+    {
+        if (false === $request->isMethod(Request::METHOD_POST)) {
+            return $this->render(self::ADD);
+        }
+
+        $errors = [];
+
+        try {
+            $this->tagService->create([
+                'id' => Uuid::v4(),
+                'name' => $request->get('name'),
+            ]);
+
+            $this->addFlash(FlashMessageTypeEnum::SUCCESS->value, $this->translator->trans('view.tag.message.created'));
+        } catch (ValidatorException $exception) {
+            $errors = $exception->getConstraintViolationList();
+        } catch (Exception $exception) {
+            $errors = [$exception->getMessage()];
+        }
+
+        if (false === empty($errors)) {
+            return $this->render(self::ADD, ['errors' => $errors]);
+        }
+
+        return $this->redirectToRoute('admin_tag_list');
     }
 }
