6c304e157fcec6906f054ab081118c6cfb099e02
Alessandro Feitoza <alessandrofeitoza01@gmail.com>
2025-02-28 15:51:23 -0300

    Using aurora-ui as CDN (#151)

--- assets/app.js
+++ assets/app.js
@@ -1,17 +1,3 @@
-import 'aurora-user-interface/dist/css/bootstrap.min.css';
-import 'aurora-user-interface/dist/css/colors.css';
-import 'aurora-user-interface/dist/css/layout.css';
-
-import 'aurora-user-interface/dist/css/accordion.css';
-import 'aurora-user-interface/dist/css/card.css';
-import 'aurora-user-interface/dist/css/editor.css';
-import 'aurora-user-interface/dist/css/faq.css';
-import 'aurora-user-interface/dist/css/forms.css';
-import 'aurora-user-interface/dist/css/navigation.css';
-import 'aurora-user-interface/dist/css/side-filter.css';
-import 'aurora-user-interface/dist/css/timeline.css';
-import 'aurora-user-interface/dist/css/snackbar.css';
-
 import './styles/app.css';
 import './styles/components/navbar.css';
 import './styles/components/footer.css';
@@ -23,10 +9,4 @@ import './styles/components/form-step.css';
 
 import './js/navbar-dropdown.js';
 
-import 'aurora-user-interface/dist/js/bootstrap.min.js';
-
-import '@iconify/iconify';
-
-import { Toast } from 'aurora-user-interface';
-
-export { Toast };
+import '@iconify/iconify'
