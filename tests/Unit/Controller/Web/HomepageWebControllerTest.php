2436a8b4bc3c173a917354d292b97272cb322029
Henrique Lopes Lima <38414109+henriquelopeslima@users.noreply.github.com>
2025-03-07 14:50:29 -0300

    Increase tests for HomepageWebController (#204)

---
+++ tests/Unit/Controller/Web/HomepageWebControllerTest.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Unit\Controller\Web;
+
+use App\Controller\Web\HomepageWebController;
+use App\Tests\AbstractWebTestCase;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Contracts\Translation\TranslatorInterface;
+
+class HomepageWebControllerTest extends AbstractWebTestCase
+{
+    private HomepageWebController $controller;
+    private TranslatorInterface $translator;
+
+    protected function setUp(): void
+    {
+        $this->controller = static::getContainer()->get(HomepageWebController::class);
+        $this->translator = static::getContainer()->get(TranslatorInterface::class);
+    }
+
+    public function testHomePageExists(): void
+    {
+        $this->assertInstanceOf(Response::class, $this->controller->homepage());
+    }
+
+    public function testHomePageRenderHTMLWithSuccess(): void
+    {
+        $client = static::webClient();
+        $client->request('GET', '/');
+
+        $this->assertResponseIsSuccessful();
+        $this->assertSelectorTextContains('h1', $this->translator->trans('view.homepage.title'));
+    }
+}
