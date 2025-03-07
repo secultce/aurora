43ef8f097ba32d32e80061e07107ff98cea77d87
Talyson Soares <talysonsoares40@gmail.com>
2025-03-06 23:37:49 -0300

    Register space type page (#164)

---
+++ cypress/e2e/admin/space-type/space-type-create.cy.js
@@ -0,0 +1,22 @@
+describe('Teste para Criar Tipo de espaço', () => {
+    beforeEach(() => {
+        cy.viewport(1920, 1080);
+        cy.login('talysonsoares@example.com', 'Aurora@2024');
+        cy.visit('/painel/admin/tipo-de-espaco/adicionar');
+    });
+
+    it('Cadastrar um novo tipo de espaço', () => {
+        cy.get('[data-cy="name"]').should('exist');
+
+        cy.get('[data-cy="name"]').type('Lupanar');
+        cy.get('[type="submit"]').click();
+
+        cy.contains('Criar').click();
+
+        cy.get('[data-cy="name"]').should('exist');
+
+        cy.get('[data-cy="name"]').type('P');
+        cy.get('[type="submit"]').click();
+        cy.get('.danger.snackbar').contains('O valor é muito curto. Deveria de ter 2 caracteres ou mais.').should('be.visible');
+    });
+});
--- src/Controller/Web/Admin/SpaceTypeAdminController.php
+++ src/Controller/Web/Admin/SpaceTypeAdminController.php
@@ -4,15 +4,23 @@ declare(strict_types=1);
 
 namespace App\Controller\Web\Admin;
 
+use App\Exception\ValidatorException;
 use App\Service\Interface\SpaceTypeServiceInterface;
+use Exception;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Uid\Uuid;
+use Symfony\Contracts\Translation\TranslatorInterface;
 
 class SpaceTypeAdminController extends AbstractAdminController
 {
     private const string LIST = 'space-type/list.html.twig';
+    private const string CREATE = 'space-type/create.html.twig';
+    private const string CREATE_FORM_ID = 'add-space-type';
 
     public function __construct(
         private readonly SpaceTypeServiceInterface $service,
+        private readonly TranslatorInterface $translator,
     ) {
     }
 
@@ -23,5 +31,38 @@ class SpaceTypeAdminController extends AbstractAdminController
         return $this->render(self::LIST, [
             'spaceTypes' => $spaceTypes,
         ]);
+    }
+
+    public function create(Request $request): Response
+    {
+        if (false === $request->isMethod(Request::METHOD_POST)) {
+            return $this->render(self::CREATE, [
+                'form_id' => self::CREATE_FORM_ID,
+            ]);
+        }
+
+        $errors = [];
+
+        try {
+            $this->service->create([
+                'id' => Uuid::v4(),
+                'name' => $request->get('name'),
+            ]);
+
+            $this->addFlashSuccess($this->translator->trans('view.space_type.message.created'));
+        } catch (ValidatorException $exception) {
+            $errors = $exception->getConstraintViolationList();
+        } catch (Exception $exception) {
+            $errors = [$exception->getMessage()];
+        }
+
+        if (false === empty($errors)) {
+            return $this->render(self::CREATE, [
+                'errors' => $errors,
+                'form_id' => self::CREATE_FORM_ID,
+            ]);
+        }
+
+        return $this->redirectToRoute('admin_space_type_list');
     }
 }
