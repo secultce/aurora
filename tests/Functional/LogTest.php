f8a30207c4d80a440658df441edf6d8247f82f32
Ronny John <me@ronnyjohnti.dev>
2025-03-10 08:43:00 -0300

    Add tests to Log class (#656)

--- tests/Functional/LogTest.php
+++ tests/Functional/LogTest.php
@@ -4,6 +4,10 @@ declare(strict_types=1);
 
 namespace App\Tests\Functional;
 
+use App\Log\Log;
+use Psr\Log\LoggerInterface;
+use ReflectionClass;
+use RuntimeException;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 use Symfony\Component\Filesystem\Filesystem;
 
@@ -31,7 +35,7 @@ class LogTest extends KernelTestCase
         }
     }
 
-    public function testLog(): void
+    public function testLogFile(): void
     {
         $logger = self::getContainer()->get('monolog.logger');
 
@@ -48,6 +52,84 @@ class LogTest extends KernelTestCase
             $log = file_get_contents($this->logsDir.'/'.$file);
             $this->assertStringContainsString($key, $log);
         }
+    }
+
+    public function testInitLogger(): void
+    {
+        $logger = $this->createMock(LoggerInterface::class);
+
+        Log::init($logger);
+
+        $this->assertInstanceOf(LoggerInterface::class, $this->getPrivateStaticProperty(Log::class, 'logger'));
+    }
+
+    public function testGetLoggerThrowsExceptionWhenNotInitialized(): void
+    {
+        $this->resetLogger();
+        $this->expectException(RuntimeException::class);
+        $this->expectExceptionMessage('Logger has not been initialized.');
+
+        $this->invokePrivateStaticMethod(Log::class, 'getLogger');
+    }
+
+    public function testCriticalMethodCallsLogger(): void
+    {
+        $this->assertLogMethodCalled('critical');
+    }
+
+    public function testDebugMethodCallsLogger(): void
+    {
+        $this->assertLogMethodCalled('debug');
+    }
+
+    public function testErrorMethodCallsLogger(): void
+    {
+        $this->assertLogMethodCalled('error');
+    }
+
+    public function testInfoMethodCallsLogger(): void
+    {
+        $this->assertLogMethodCalled('info');
+    }
+
+    private function assertLogMethodCalled(string $method): void
+    {
+        $logger = $this->createMock(LoggerInterface::class);
+        $logger->expects($this->once())
+            ->method($method)
+            ->with(
+                $this->equalTo('Test message'),
+                $this->equalTo(['key' => 'value'])
+            );
+
+        Log::init($logger);
+        Log::$method('Test message', ['key' => 'value']);
+    }
+
+    private function getPrivateStaticProperty(string $class, string $property): mixed
+    {
+        $reflection = new ReflectionClass($class);
+        $prop = $reflection->getProperty($property);
+        $prop->setAccessible(true);
+
+        return $prop->getValue();
+    }
+
+    private function invokePrivateStaticMethod(string $class, string $method): mixed
+    {
+        $reflection = new ReflectionClass($class);
+        $method = $reflection->getMethod($method);
+        $method->setAccessible(true);
+
+        return $method->invoke(null);
+    }
+
+    private function resetLogger(): void
+    {
+        $reflection = new ReflectionClass(Log::class);
+        $prop = $reflection->getProperty('logger');
+        $prop->setAccessible(true);
+        $prop->setValue(null);
     }
 
     protected function tearDown(): void
