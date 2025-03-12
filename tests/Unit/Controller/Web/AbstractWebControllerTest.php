9d0b33e5e5cacdcb0011edad4ea83640f873daea
Igor Mendes <a.igor.mendes@gmail.com>
2025-03-12 10:53:59 -0300

    Create unit test to Abstract Web Controller (#214)

--- tests/Unit/Controller/Web/AbstractWebControllerTest.php
+++ tests/Unit/Controller/Web/AbstractWebControllerTest.php
@@ -6,6 +6,12 @@ namespace App\Tests\Unit\Controller\Web;
 
 use App\Controller\Web\AbstractWebController;
 use App\Tests\AbstractWebTestCase;
+use Symfony\Bundle\FrameworkBundle\KernelBrowser;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Component\Security\Core\Exception\InvalidCsrfTokenException;
+use Symfony\Component\Security\Csrf\CsrfTokenManager;
+use Symfony\Component\Security\Csrf\TokenStorage\SessionTokenStorage;
 
 class AbstractWebControllerTest extends AbstractWebTestCase
 {
@@ -14,6 +20,10 @@ class AbstractWebControllerTest extends AbstractWebTestCase
     protected function setUp(): void
     {
         $this->controller = new class extends AbstractWebController {
+            public function validCsrfToken(string $tokenId, Request $request): void
+            {
+                parent::validCsrfToken($tokenId, $request);
+            }
         };
     }
 
@@ -57,5 +67,121 @@ class AbstractWebControllerTest extends AbstractWebTestCase
         $this->assertEquals('DESC', $result['order']);
         $expectedFilters = ['name' => 'Talyson'];
         $this->assertEquals($expectedFilters, $result['filters']);
+    }
+
+    public function testShouldNotThrowExceptionForAValidToken(): void
+    {
+        $tokenId = 'csrf_token_id_test';
+
+        self::ensureKernelShutdown();
+        $client = self::createClient();
+
+        $token = $this->getValidToken($tokenId, $client);
+
+        $request = new Request(request: ['token' => $token]);
+        $request->setMethod(Request::METHOD_POST);
+
+        $container = $client->getContainer();
+        $this->controller->setContainer($container);
+
+        try {
+            $this->controller->validCsrfToken($tokenId, $request);
+        } catch (InvalidCsrfTokenException) {
+            $this->fail('Failed to assert that an InvalidCsrfTokenException is not thrown.');
+        }
+
+        $this->addToAssertionCount(1);
+    }
+
+    public function testMustThrowExceptionWhenATokenIsWrong(): void
+    {
+        $tokenId = 'csrf_token_id_test';
+        $wrongTokenid = 'wrong_token_id';
+
+        self::ensureKernelShutdown();
+        $client = self::createClient();
+
+        $token = $this->getValidToken($wrongTokenid, $client);
+
+        $request = new Request(request: ['token' => $token]);
+        $request->setMethod(Request::METHOD_POST);
+
+        $container = $client->getContainer();
+        $this->controller->setContainer($container);
+
+        $this->expectException(InvalidCsrfTokenException::class);
+
+        $container = $client->getContainer();
+        $this->controller->setContainer($container);
+        $this->controller->validCsrfToken($tokenId, $request);
+    }
+
+    public function testMustThrowExceptionWhenATokenIsSentFromADiferentSession(): void
+    {
+        $tokenId = 'csrf_token_id_test';
+
+        $token = $this->getValidTokenFromAnotherSession($tokenId);
+
+        self::ensureKernelShutdown();
+        $client = self::createClient();
+
+        $client->request(Request::METHOD_GET, '/');
+
+        $request = $client->getRequest();
+        $request->getSession()->start();
+
+        /** @var RequestStack $requestStack */
+        $requestStack = $client->getContainer()->get(RequestStack::class);
+        $requestStack->push($request);
+
+        new SessionTokenStorage($requestStack);
+
+        $request = new Request(request: ['token' => $token]);
+        $request->setMethod(Request::METHOD_POST);
+
+        $this->expectException(InvalidCsrfTokenException::class);
+
+        $container = $client->getContainer();
+        $this->controller->setContainer($container);
+        $this->controller->validCsrfToken($tokenId, $request);
+    }
+
+    private function getValidToken(string $tokenId, KernelBrowser $client): string
+    {
+        $client->request(Request::METHOD_GET, '/');
+
+        $request = $client->getRequest();
+        $request->getSession()->start();
+
+        /** @var RequestStack $requestStack */
+        $requestStack = $client->getContainer()->get(RequestStack::class);
+        $requestStack->push($request);
+
+        $storage = new SessionTokenStorage($requestStack);
+
+        $tokenManager = new CsrfTokenManager(storage: $storage);
+
+        return $tokenManager->getToken($tokenId)->getValue();
+    }
+
+    private function getValidTokenFromAnotherSession(string $tokenId): string
+    {
+        self::ensureKernelShutdown();
+        $client = self::createClient();
+
+        $client->request(Request::METHOD_GET, '/');
+
+        $request = $client->getRequest();
+        $request->getSession()->start();
+
+        /** @var RequestStack $requestStack */
+        $requestStack = $client->getContainer()->get(RequestStack::class);
+        $requestStack->push($request);
+
+        $storage = new SessionTokenStorage($requestStack);
+
+        $tokenManager = new CsrfTokenManager(storage: $storage);
+
+        return $tokenManager->getToken($tokenId)->getValue();
     }
 }
